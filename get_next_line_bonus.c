3d5480432213968ce7197faea4b1ff005b78e0ad
Filipe Lima <flima@f0r4s21.codam.nl>
2024-11-26 20:23:31 +0100

    bonus files added

---
+++ get_next_line_bonus.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flima <flima@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 15:48:58 by mahkilic          #+#    #+#             */
+/*   Updated: 2024/11/26 20:21:33 by flima            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static char	*clean_storage(char *storage)
+{
+	char	*new_storage;
+	char	*ptr;
+	int		len;
+
+	ptr = ft_strchr(storage, '\n');
+	if (!ptr)
+		return (free(storage), storage = NULL, new_storage = NULL);
+	else
+		len = (ptr - storage) + 1;
+	if (!storage[len])
+		return (free(storage), storage = NULL);
+	new_storage = ft_substr(storage, len, ft_strlen(storage) - len);
+	(free(storage), storage = NULL);
+	if (!new_storage)
+		return (NULL);
+	return (new_storage);
+}
+
+static char	*new_line(char *storage)
+{
+	char	*line;
+	char	*ptr;
+	int		len;
+
+	ptr = ft_strchr(storage, '\n');
+	len = (ptr - storage) + 1;
+	line = ft_substr(storage, 0, len);
+	if (!line)
+		return (NULL);
+	return (line);
+}
+
+static char	*read_buffer(int fd, char *storage)
+{
+	int		rid;
+	char	*buffer;
+
+	rid = 1;
+	buffer = malloc((BUFFER_SIZE + 1) * 1);
+	if (!buffer)
+		return (free(storage), storage = NULL, NULL);
+	buffer[0] = '\0';
+	while (rid > 0 && !ft_strchr(buffer, '\n'))
+	{
+		rid = read (fd, buffer, BUFFER_SIZE);
+		if (rid > 0)
+		{
+			buffer[rid] = '\0';
+			storage = ft_strjoin(storage, buffer);
+		}
+	}
+	free(buffer);
+	if (rid == -1)
+		return (free(storage), storage = NULL, NULL);
+	return (storage);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*storage[1024];
+	char		*line;
+
+	if (fd < 0)
+		return (NULL);
+	if ((storage[fd] && !ft_strchr(storage[fd], '\n')) || !storage[fd])
+		storage[fd] = read_buffer (fd, storage[fd]);
+	if (!storage[fd])
+		return (NULL);
+	line = new_line(storage[fd]);
+	if (!line)
+		return (free(storage[fd]), storage[fd] = NULL, NULL);
+	storage[fd] = clean_storage(storage[fd]);
+	return (line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flima <flima@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 15:43:59 by mahkilic          #+#    #+#             */
+/*   Updated: 2024/11/26 20:15:18 by flima            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+
+char	*ft_strchr(char *s, int c);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+size_t	ft_strlen(char *s);
+
+#endif
